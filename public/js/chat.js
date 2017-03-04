9c3f30c71932b7fcad8dc01be897c4cd7270d885
kamson <kamson785@gmail.com>
2017-03-04 17:10:35 +0100

    UI upadate

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,16 @@
-  
-var socket = io.connect('http://localhost:3000');
-$('form').submit(function(){
-    socket.emit('chat message',$('#message').val());
-    $('#message').val('');
-    return false;
-});
-socket.on('chat message',function(msg){
-    console.log(msg);
-    $('#messages').append($('<li>').text(msg));
-});  
+  var socket = io.connect('http://localhost:3000');
+  $('form').submit(function () {
+      if($('#message').val() != '') {
+          socket.emit('chat message', $('#message').val());
+          $('#message').val('');
+          return false;
+      }
+      else
+      {
+          return false;
+      }
+  });
+  socket.on('chat message', function (msg) {
+      console.log(msg);
+      $('#messages').append($('<li>').text(msg));
+  });
